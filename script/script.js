8fe3c5564161c4a22c7e0ee071d2e97e7ec48f35
Darktaste <vasil.lyubenov@gmail.com>
2022-08-10 22:02:20 +0300

    basic HTML structure and CSS, JS files

---
+++ script/script.js
@@ -0,0 +1,7 @@
+function createGrid(){
+    const container = document.querySelector('.container');
+    const cell = document.createElement('div');
+    container.appendChild(cell);
+}
+
+createGrid();
