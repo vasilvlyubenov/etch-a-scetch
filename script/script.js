d441166f24f80308a9af8c70916a56cf83d51f57
Darktaste <vasil.lyubenov@gmail.com>
2022-08-13 20:10:18 +0300

    created grid

--- script/script.js
+++ script/script.js
@@ -1,7 +1,24 @@
-function createGrid(){
-    const container = document.querySelector('.container');
-    const cell = document.createElement('div');
-    container.appendChild(cell);
+const range = document.getElementById('range');
+const container = document.querySelector('.container');
+
+function createGrid(number) {
+    
+    for(let i = 1; i <= number * number; i++) {
+        console.log(range);
+        
+        const grid = document.createElement('div');    
+        grid.classList.add('grid');
+        container.style.gridTemplateColumns = `repeat(${number}, 1fr)`;
+        container.style.gridTemplateRows = `repeat(${number}, 1fr)`;
+        container.insertAdjacentElement('beforeend', grid);
+}   
 }
 
-createGrid();
+
+function pixelSize() {
+    let gridPixels = container.querySelectorAll('div');
+    gridPixels.forEach(gridPixel => gridPixel.remove());
+    createGrid(range.value);
+}
+
+range.addEventListener('mouseup', pixelSize);
